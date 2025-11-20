716b71a493cc9e395d1a412363296faa5849692c
Connor Eaves <kishotta@gmail.com>
2025-11-19 19:13:36 -0600

    Fixes HTML entity in profile summary
    Replaces the HTML entity `>` with `>` in the profile summary for correct rendering.

--- my-app/src/app/resume/resume.ts
+++ my-app/src/app/resume/resume.ts
@@ -25,7 +25,7 @@ export class ResumeComponent {
 
   // Profile Summary
   profile =
-    'Software Engineer with &gt;6 years of experience in developing scalable, observable, robust web applications, and staying up to date with .Net ecosystem best practices, trends, developments, and innovations. My goal is to use my knowledge and abilities to deliver high quality software that helps improve the lives of my users by providing useful, intuitive, and secure tools to enhance their productivity, and provide necessary services. I focus on predictable, clean, maintainable, self-documenting, extensible code that ensures a positive developer experience for anyone sharing a codebase with me.';
+    'Software Engineer with >6 years of experience in developing scalable, observable, robust web applications, and staying up to date with .Net ecosystem best practices, trends, developments, and innovations. My goal is to use my knowledge and abilities to deliver high quality software that helps improve the lives of my users by providing useful, intuitive, and secure tools to enhance their productivity, and provide necessary services. I focus on predictable, clean, maintainable, self-documenting, extensible code that ensures a positive developer experience for anyone sharing a codebase with me.';
 
   // Skills
   skills = {
